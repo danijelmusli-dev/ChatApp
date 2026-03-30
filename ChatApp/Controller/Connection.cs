45ff03fc79d5de0bb29cff86f02018f3fa4e5d6d
Danijel Musli <danijelmusli56@gmail.com>
2026-03-30 21:03:05 +0200

    Implemented UI Design - implemented custom User Controls - implemented design for custom User Controls - necessary classes added - necessary models added

---
+++ ChatApp/Controller/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Controller
+{
+    public class Connection
+    {
+        public string Name { get; set; }
+            public string IP { get; set; }
+            public int Port { get; set; }
+            public bool IsConnected { get; set; }
+
+            public Connection(string name, string ip, int port)
+            {
+                Name = name;
+                IP = ip;
+                Port = port;
+                IsConnected = false;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name} ({IP}:{Port}) - {(IsConnected ? "Connected" : "Disconnected")}";
+            }
+    }
+}
---
+++ ChatApp/Controller/ConnectionList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ChatApp.Controller
+{
+    public class ConnectionList
+    {
+        public IObservable<Connection> Connections { get; private set; }
+    }
+}
